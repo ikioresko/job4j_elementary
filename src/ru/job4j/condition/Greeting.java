713ee17f522cad58250559715452bee117b61901
ikioresko <ikioresko@gmail.com>
2020-11-25 13:51:35 +0500

    9.1. String [#418158]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -1,9 +1,10 @@
 package ru.job4j.condition;
 
 public class Greeting {
+    @SuppressWarnings("checkstyle:WhitespaceAround")
     public static void main(String[] args) {
         String idea = "I like Java!";
         int year = 2020;
-        System.out.println(idea + " But I am a newbie. "+ year);
+        System.out.println(idea + " But I am a newbie. " + year);
     }
 }
