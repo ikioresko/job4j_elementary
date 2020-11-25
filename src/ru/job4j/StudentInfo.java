89416749645ae0eba6111a57371579663771e75d
ikioresko <ikioresko@gmail.com>
2020-11-25 09:56:31 +0500

    3.3. Разбор первой программы. [#418168]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Игорь Киореско");
+        System.out.println("02.06.1991");
+    }
+}
