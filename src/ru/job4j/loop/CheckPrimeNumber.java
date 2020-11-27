3278ee12f7bf48176dbf71c75fdfcf34964914eb
ikioresko <ikioresko@gmail.com>
2020-11-27 16:00:04 +0500

    5.5. Простое число [#418208]

---
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public static boolean check(int number) {
+        boolean prime = false;
+        for (int i = 2; i < number; i++) {
+            if (i < (number - 1)) {
+                if (number % i == 0) {
+                    break;
+                }
+            } else {
+                prime = true;
+            }
+        }
+        return prime;
+    }
+}
