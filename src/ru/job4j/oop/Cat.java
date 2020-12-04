03b1a1c9ba291e8fabb330f99cabae53b555fa01
ikioresko <ikioresko@gmail.com>
2020-12-04 11:13:32 +0500

    1.5. Поля объекта [#418309]

--- src/ru/job4j/oop/Cat.java
+++ src/ru/job4j/oop/Cat.java
@@ -1,8 +1,29 @@
 package ru.job4j.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("Кличка котика: " + this.name + ". Сегодня он ел: " + this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("Гав");
+        gav.show();
+        Cat black = new Cat();
+        black.eat("fish");
+        black.giveNick("Черный");
+        black.show();
     }
 }
