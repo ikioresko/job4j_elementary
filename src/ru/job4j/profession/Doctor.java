fa7b9fedceffd1479cf9d36797f366c7a894418c
ikioresko <ikioresko@gmail.com>
2020-12-07 17:38:29 +0500

    3. Реализация профессий в коде [#418326]

--- src/ru/job4j/profession/Doctor.java
+++ src/ru/job4j/profession/Doctor.java
@@ -1,8 +1,19 @@
 package ru.job4j.profession;
 
+import javax.print.Doc;
+
 public class Doctor extends Profession {
     private String diagnosis;
     private String inspect;
+
+    public Doctor() {
+    }
+
+    public Doctor(String name, String surname, String education, String birthday, String diagnosis, String inspect) {
+        super(name, surname, education, birthday);
+        this.diagnosis = diagnosis;
+        this.inspect = inspect;
+    }
 
     public String getDiagnosis() {
         return null;
