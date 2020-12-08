b3ed06a5cba31924fbf16eefb1b55b42ae9aeb78
ikioresko <ikioresko@gmail.com>
2020-12-08 11:59:51 +0500

    2. Модель данных. [#418328]

---
+++ src/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+
+public class College {
+    public static void main(String[] args) {
+        Student st = new Student();
+        st.setFio("Семенов Вячеслав Михайлович");
+        st.setGroupNum(515);
+        st.setRegDate("5 февряля 2005 года");
+        System.out.println("ФИО : " + st.getFio() + System.lineSeparator() + "Номер группы : " + st.getGroupNum() + System.lineSeparator() + "Дата поступления : " + st.getRegDate());
+    }
+}
---
+++ src/ru/job4j/pojo/Student.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Student {
+    private String fio;
+    private int groupNum;
+    private String regDate;
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public int getGroupNum() {
+        return groupNum;
+    }
+
+    public void setGroupNum(int groupNum) {
+        this.groupNum = groupNum;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+}
